23350389859ae47cfc96d0be99051a110036588d
miya <tukiya8@yahoo.co.jp>
2025-03-12 21:50:16 +0900

    演習講座3−4〜3−7

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -31,8 +31,15 @@
         @csrf
         <div class="create-form__item">
             <input class="create-form__item-input" type="text" name="content" value="{{ old('content') }}">
-            <select class="create-form__item-select">
-                <option value="">カテゴリ</option>
+            <!-- <select class="create-form__item-select" name="category_id">
+                <option value="">カテゴリ</option> -->
+            <!-- <option value="1">カテゴリ1</option> -->
+            <!-- <option value="2">カテゴリ2</option>
+                <option value="3">カテゴリ3</option> -->
+            <select class="create-form__item-select" name="category_id">
+                @foreach ($categories as $category)
+                <option value="{{ $category['id'] }}">{{ $category['name'] }}</option>
+                @endforeach
             </select>
         </div>
         <div class="create-form__button">
@@ -42,11 +49,18 @@
     <div class="section__title">
         <h2>Todo検索</h2>
     </div>
-    <form class="search-form">
+    <!-- <form class="search-form"> -->
+    <form class="search-form" action="/todos/search" method="get">
+        @csrf
         <div class="search-form__item">
-            <input class="search-form__item-input" type="text" />
-            <select class="search-form__item-select">
+            <!-- <input class="search-form__item-input" type="text" /> -->
+            <input class="search-form__item-input" type="text" name="keyword" value="{{ old('keyword') }}">
+            <!-- <select class="search-form__item-select"> -->
+            <select class="search-form__item-select" name="category_id">
                 <option value="">カテゴリ</option>
+                @foreach ($categories as $category)
+                <option value="{{ $category['id'] }}">{{ $category['name'] }}</option>
+                @endforeach
             </select>
         </div>
         <div class="search-form__button">
@@ -76,7 +90,8 @@
                             <input type="hidden" name="id" value="{{ $todo['id'] }}">
                         </div>
                         <div class="update-form__item">
-                            <p class="update-form__item-p">Category 1</p>
+                            <!-- <p class="update-form__item-p">Category 1</p> -->
+                            <p class="update-form__itme-p">{{ $todo['category']['name'] }}</p>
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
